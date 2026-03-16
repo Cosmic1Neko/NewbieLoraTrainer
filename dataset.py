5139450cf0300cedbf408f8211939c7a878c0fbb
Cosmic1Neko <132188345+Cosmic1Neko@users.noreply.github.com>
2026-03-16 19:53:52 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -197,8 +197,7 @@ class ImageCaptionDataset(Dataset):
                         pixel_values = transform(image).unsqueeze(0).to(self.device)
                         with torch.no_grad():
                             pixel_values_5d = pixel_values.unsqueeze(2).to(self.dtype)
-                            vae_scale = [s.to(device=pixel_values_5d.device) for s in self.vae.scale]
-                            latents = self.vae.model.encode(pixel_values_5d, vae_scale)
+                            latents = self.vae.model.encode(pixel_values_5d, vae.scale)
                             latents = latents.squeeze(2).squeeze(0).cpu()
 
                         save_file({
